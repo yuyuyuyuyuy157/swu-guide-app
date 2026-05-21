0eb09cfdbe052c5429f497a286e9d92a91290566
Lan518 <wy7817988@gmail.com>
2026-05-21 12:35:01 +0800

    错误信息

---
+++ nav-common/src/main/java/com/nav/constant/MessageConstant.java
@@ -0,0 +1,9 @@
+package com.nav.constant;
+
+public class MessageConstant {
+    public static final String PASSWORD_ERROR = "密码错误";
+    public static final String ACCOUNT_NOT_FOUND = "账号不存在";
+    public static final String ACCOUNT_LOCKED = "账号已被冻结";
+    public static final String PHONE_EXISTS = "该手机号已被注册";
+    public static final String INVALID_CODE = "邀请码无效、已过期或已被使用";
+}
